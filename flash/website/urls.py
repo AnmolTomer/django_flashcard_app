f7136175dc28c918e36e8e4b950a70059c3fb145
Anmol <anmol3540@gmail.com>
2020-03-31 01:03:33 +0530

    Add url, view and template for homepage.

--- flash/website/urls.py
+++ flash/website/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views  # From this directory import views file
 
 urlpatterns = [
-
+    path('', views.home, name="home"),
 ]
--- flash/website/views.py
+++ flash/website/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# View for home
+def home(request):
+    return render(request, 'home.html', {})
