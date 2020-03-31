848a02792f338b0ee158fd362894126ba95e81e1
Anmol <anmol3540@gmail.com>
2020-03-31 09:18:54 +0530

    Add template, URL, views for math web pages.

--- flash/website/views.py
+++ flash/website/views.py
@@ -6,3 +6,19 @@ from django.shortcuts import render
 # View for home
 def home(request):
     return render(request, 'home.html', {})
+
+
+def add(request):
+    return render(request, 'add.html', {})
+
+
+def subtract(request):
+    return render(request, "subtract.html", {})
+
+
+def multiply(request):
+    return render(request, "multiply.html", {})
+
+
+def divide(request):
+    return render(request, "divide.html", {})
